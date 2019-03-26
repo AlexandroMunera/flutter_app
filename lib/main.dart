815fe3748596dfbff3e0216c189084e9f4b6e248
Freud Munera <alexandromunera@gmail.com>
2019-03-26 15:42:04 +0100

    First Page

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,142 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
+    /* return MaterialApp(
         title: 'Startup Name Generator',
         theme: new ThemeData(
           primaryColor: Colors.grey[900],
         ),
         home: RandomWords()
+    );*/
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder>{
+        '/signup': (BuildContext context) => InitialPage()
+      },
+      home: InitialPage(),
     );
+  }
+}
+
+class InitialPage extends StatefulWidget {
+  @override
+  _InitialPageState createState() => _InitialPageState();
+}
+
+class _InitialPageState extends State<InitialPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        backgroundColor: Colors.white,
+        resizeToAvoidBottomPadding: false,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(30.0, 110.0, 0.0, 0.0),
+                    child: Text('Welcome to Gdcoin your',
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            //fontWeight: FontWeight.bold,
+                            fontFamily: 'Gilroy-Bold'
+                        )
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(40.0, 140.0, 0.0, 0.0),
+                    child: Text('smart crypto exchange',
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            //fontWeight: FontWeight.bold,
+                            fontFamily: 'Gilroy-Bold'
+                        )
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                width:  400.0,
+                height: 300.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/initialPageImage.png")),
+                )),
+            Container(
+                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                child: Column(
+                  children: <Widget>[
+
+                    Container(
+                      height: 40.0,
+                      child: Material(
+                        borderRadius: BorderRadius.circular(20.0),
+                        shadowColor: Colors.greenAccent,
+                        color: Color(0xFFF29C1F),
+                        elevation: 7.0,
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Center(
+                            child: Text(
+                              'Register',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Gilroy-Bold'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Container(
+                      height: 40.0,
+                      color: Colors.transparent,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.black,
+                                style: BorderStyle.solid,
+                                width: 1.0),
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.circular(20.0)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Center(
+                              child:
+                                  ImageIcon(AssetImage('assets/facebook.png')),
+                            ),
+                            SizedBox(width: 10.0),
+                            Center(
+                              child: Text('Login',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Gilroy-Bold')),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )),
+            SizedBox(height: 15.0),
+            Center(
+              child: Text(
+                'Read terms and privacy policy',
+                
+                style: TextStyle(
+                    fontFamily: 'Gilroy-Medium',
+                    color: Color(0xFF888888)
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
 
@@ -29,7 +157,6 @@ class RandomWordsState extends State<RandomWords> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
@@ -84,7 +211,7 @@ class RandomWordsState extends State<RandomWords> {
       new MaterialPageRoute<void>(
         builder: (BuildContext context) {
           final Iterable<ListTile> tiles = _saved.map(
-                (WordPair pair) {
+            (WordPair pair) {
               return new ListTile(
                 title: new Text(
                   pair.asPascalCase,
@@ -93,12 +220,10 @@ class RandomWordsState extends State<RandomWords> {
               );
             },
           );
-          final List<Widget> divided = ListTile
-              .divideTiles(
+          final List<Widget> divided = ListTile.divideTiles(
             context: context,
             tiles: tiles,
-          )
-              .toList();
+          ).toList();
 
           return new Scaffold(
             appBar: new AppBar(
@@ -110,5 +235,4 @@ class RandomWordsState extends State<RandomWords> {
       ),
     );
   }
-
 }
